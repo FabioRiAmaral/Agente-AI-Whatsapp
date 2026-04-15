57dd6ba1b9ae164a34bb6a1f905f81b1c319868e
FabioRiAmaral <fabioamaralpk@gmail.com>
2026-04-15 18:14:59 -0300

    logging adicionado no main para melhor debug, recebo a mensagem e o log aparece que foi recebida, porém ainda não esta funcionando

--- src/main.py
+++ src/main.py
@@ -1,13 +1,22 @@
 from flask import Flask, request
 from bot import Bot
+import logging, sys
+
+logging.basicConfig(
+  format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+  level=logging.INFO,
+  handlers=[
+    logging.StreamHandler(sys.stdout)
+  ]
+)
 
 app = Flask(__name__)
 bot = Bot()
 
-@app.route("/webhook", methods=["POST"])
+@app.route("/webhook", methods=["POST"]) 
 def webhook():
   bot.handleEvent(request.json)
   return "Tudo certo"
 
 if __name__ == "__main__":
-  app.run(host="0.0.0.0", port=5000, debug=False)
+  app.run(host="0.0.0.0", port=5000, debug=False) # http://host.docker.internal:5000/webhook
